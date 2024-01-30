523a83db9aa30b0ec2bdac2c0b6075236c4c1595
Roman Karasev NB <progressivenation12@gmail.com>
2024-01-31 00:29:45 +0300

    Creating a repository for training in Kata Academy

--- src/main/java/org/javaCore/Task_1_2_4.java
+++ src/main/java/org/javaCore/Task_1_2_4.java
@@ -8,7 +8,16 @@ public class Task_1_2_4 {
         long varLong = 9223372036854775807L;
         double varDouble = 1.7976931348623157E+308;
         float varFloat = 3.40282346638528860e+38F;
-        char varchar = 'V';
+        char varChar = 'V';
         boolean varBoolean = true;
+
+        System.out.println(varByte);
+        System.out.println(varShort);
+        System.out.println(varInt);
+        System.out.println(varLong);
+        System.out.println(varDouble);
+        System.out.println(varFloat);
+        System.out.println(varChar);
+        System.out.println(varBoolean);
     }
 }
