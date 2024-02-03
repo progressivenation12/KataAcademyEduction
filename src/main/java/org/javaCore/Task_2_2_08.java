aba5050b273a7441f9bf9d6e044d32a7909f6c7c
Roman Karasev <progressivenation12@gmail.com>
2024-02-03 21:58:11 +0300

    add new Task_2_2_07, Task_2_2_08, finished block 2.2 Classes and methods

---
+++ src/main/java/org/javaCore/Task_2_2_08.java
@@ -0,0 +1,33 @@
+package org.javaCore;
+
+public class Task_2_2_08 {
+    public static void main(String[] args) {
+        System.out.printf("Количество будних дней: %s, выходных дней: %s.",
+                weekdayCount(new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}),
+                weekendCount(new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}));
+    }
+
+    public static boolean isWeekend(String dayName) {
+        return dayName.equals("Saturday") || dayName.equals("Sunday");
+    }
+
+    public static int weekendCount(String[] days) {
+        int counterWeekend = 0;
+        for (String day : days) {
+            if (isWeekend(day)) {
+                counterWeekend++;
+            }
+        }
+        return counterWeekend;
+    }
+
+    public static int weekdayCount(String[] days) {
+        int counterWeekday = 0;
+        for (String day : days) {
+            if (!isWeekend(day)) {
+                counterWeekday++;
+            }
+        }
+        return counterWeekday;
+    }
+}
