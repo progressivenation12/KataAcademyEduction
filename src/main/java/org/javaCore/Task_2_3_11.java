4513cd9d8ae4f70ddb3dd91053ecce219df45473
Roman Karasev <progressivenation12@gmail.com>
2024-02-04 23:27:50 +0300

    change Task_2_3_11 add new Task_2_4_09, Task_2_4_11

--- src/main/java/org/javaCore/Task_2_3_11.java
+++ src/main/java/org/javaCore/Task_2_3_11.java
@@ -9,35 +9,35 @@ public class Task_2_3_11 {
         System.out.println(nameIvan.getGossip());
     }
 
+    static class Human {
+        private final String secret; //секретики
+        private final String news; //новости
+        private final String experience; //опыт
+        private final String gossip; //сплетни
+
+        public Human(String secret, String news, String experience, String gossip) {
+            this.secret = secret;
+            this.news = news;
+            this.experience = experience;
+            this.gossip = gossip;
+        }
+
+        private String getSecret() {
+            return secret;
+        }
+
+        public String getNews() {
+            return news;
+        }
+
+        protected String getExperience() {
+            return experience;
+        }
+
+        String getGossip() {
+            System.out.println(getSecret());
+            return gossip;
+        }
+    }
 }
 
-class Human {
-    private final String secret; //секретики
-    private final String news; //новости
-    private final String experience; //опыт
-    private final String gossip; //сплетни
-
-    public Human(String secret, String news, String experience, String gossip) {
-        this.secret = secret;
-        this.news = news;
-        this.experience = experience;
-        this.gossip = gossip;
-    }
-
-    private String getSecret() {
-        return secret;
-    }
-
-    public String getNews() {
-        return news;
-    }
-
-    protected String getExperience() {
-        return experience;
-    }
-
-    String getGossip() {
-        System.out.println(getSecret());
-        return gossip;
-    }
-}
---
+++ src/main/java/org/javaCore/Task_2_4_11.java
@@ -0,0 +1,19 @@
+package org.javaCore;
+
+public class Task_2_4_11 {
+    public static void main(String[] args) {
+        LazyStudent lazyStudent = new LazyStudent();
+
+        lazyStudent.study();
+    }
+
+    public static class LazyStudent extends Task_2_4_09.Student {
+        public LazyStudent() {
+        }
+
+        @Override
+        public void study() {
+            System.out.println("Сегодня не учусь, мне лень.");
+        }
+    }
+}
