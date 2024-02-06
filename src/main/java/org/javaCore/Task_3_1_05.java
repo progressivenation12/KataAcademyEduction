b9de63bbb660f353d5124e19ff7323d58b34bd98
Roman Karasev <progressivenation12@gmail.com>
2024-02-06 22:20:54 +0300

    add new Task_3_1_05, Task_3_1_06, Task_3_1_07

---
+++ src/main/java/org/javaCore/Task_3_1_05.java
@@ -0,0 +1,23 @@
+package org.javaCore;
+
+import java.io.IOException;
+
+public class Task_3_1_05 {
+    public static void main(String[] args) throws MyNewException {
+        testExp();
+    }
+
+    public static void testExp() throws MyNewException {
+        throw new MyNewException("Exception");
+    }
+}
+
+class MyNewException extends IOException {
+    public MyNewException(String message) {
+        super(message);
+    }
+
+    public MyNewException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/org/javaCore/Task_3_1_07.java
@@ -0,0 +1,7 @@
+package org.javaCore;
+
+public class Task_3_1_07 {
+    public static void main(String[] args) {
+
+    }
+}
