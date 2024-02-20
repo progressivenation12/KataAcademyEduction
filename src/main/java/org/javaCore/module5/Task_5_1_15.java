3865ea4f5fb9746170560f6c1b933b0323c79f8d
nobodyRoman <progressivenation12@gmail.com>
2024-02-20 17:04:57 +0300

    add new Task_5_1_15

--- src/main/java/org/javaCore/module5/Task_5_1_15.java
+++ src/main/java/org/javaCore/module5/Task_5_1_15.java
@@ -14,45 +14,45 @@ public class Task_5_1_15 {
         System.out.println(integerDynamicArray.get(0));
     }
 
-public static class DynamicArray<T> {
-    private static final int DEFAULT_SIZE = 10;
-    private T[] array = (T[]) new Object[DEFAULT_SIZE];
-    private int arraySize = 0;
+    public static class DynamicArray<T> {
+        private static final int DEFAULT_SIZE = 10;
+        private T[] array = (T[]) new Object[DEFAULT_SIZE];
+        private int arraySize = 0;
 
-    public DynamicArray() {
-    }
-
-    public void add(T el) {
-        if (arraySize == array.length) {
-            array = Arrays.copyOf(array, array.length * 2);
-        }
-        array[arraySize] = el;
-        arraySize++;
-    }
-
-    public void remove(int index) {
-        if (index < 0 || index >= arraySize) {
-            throw new ArrayIndexOutOfBoundsException();
+        public DynamicArray() {
         }
 
-        T[] tempArray = array;
-        array = (T[]) new Object[tempArray.length - 1];
-
-        System.arraycopy(tempArray, 0, array, 0, index);
-        System.arraycopy(tempArray, index + 1, array, index, tempArray.length - index - 1);
-
-        arraySize--;
-    }
-
-    public int size() {
-        return array.length;
-    }
-
-    public T get(int index) {
-        if (index < 0 || index >= arraySize) {
-            throw new ArrayIndexOutOfBoundsException();
+        public void add(T el) {
+            if (arraySize == array.length) {
+                array = Arrays.copyOf(array, array.length * 2);
+            }
+            array[arraySize] = el;
+            arraySize++;
         }
-        return array[index];
+
+        public void remove(int index) {
+            if (index < 0 || index >= arraySize) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+
+            T[] tempArray = array;
+            array = (T[]) new Object[tempArray.length - 1];
+
+            System.arraycopy(tempArray, 0, array, 0, index);
+            System.arraycopy(tempArray, index + 1, array, index, tempArray.length - index - 1);
+
+            arraySize--;
+        }
+
+        public int size() {
+            return array.length;
+        }
+
+        public T get(int index) {
+            if (index < 0 || index >= arraySize) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            return array[index];
+        }
     }
-}
 }
